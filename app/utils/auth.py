17f2f5c4373dcea5c21a1f61068d7b36a2fd5128
rikerveja <37624420+rikerveja@users.noreply.github.com>
2024-12-18 23:02:08 +0800

    Update auth.py

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -1,18 +1,14 @@
 import bcrypt
 import jwt
 from datetime import datetime, timedelta
+from app.config import Config
 
-SECRET_KEY = "your_secret_key"  # 你可以通过 .env 文件设置并加载 SECRET_KEY
-
-# 用于加密密码
 def hash_password(password):
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
-# 用于校验密码
 def check_password(password, hashed_password):
-    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8'))
 
-# 生成 JWT Token
-def generate_jwt(payload, expiration=24):
-    payload['exp'] = datetime.utcnow() + timedelta(hours=expiration)
-    return jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+def generate_jwt(payload):
+    payload['exp'] = datetime.utcnow() + timedelta(hours=24)
+    return jwt.encode(payload, Config.SECRET_KEY, algorithm="HS256")
