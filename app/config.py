d23fbe4ea5c55d0f047d3f00119d6840f6ae6f54
rikerveja <37624420+rikerveja@users.noreply.github.com>
2024-12-19 13:58:53 +0800

    Update config.py

--- app/config.py
+++ app/config.py
@@ -27,6 +27,10 @@ class Config:
     REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
     REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
 
+    # Celery 配置
+    CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', f"redis://{REDIS_HOST}:{REDIS_PORT}/0")
+    CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', f"redis://{REDIS_HOST}:{REDIS_PORT}/0")
+
     # 日志级别（可选）
     LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO')  # 默认日志级别为 INFO
 
