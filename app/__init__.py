ae7bfe8dff088f8e6f7cf9ca119985cb8a54396d
rikerveja <37624420+rikerveja@users.noreply.github.com>
2024-12-30 15:46:49 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -50,9 +50,6 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
     
-    db.init_app(app)
-    migrate.init_app(app, db)
-
     # 设置日志
     setup_logging(app)
 
