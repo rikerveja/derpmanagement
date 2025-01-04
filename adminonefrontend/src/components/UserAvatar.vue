2b24f8f644b58dd9d6cab0212f8d9ed07fe808e5
rikerveja <37624420+rikerveja@users.noreply.github.com>
2025-01-04 15:02:02 +0800

    Update UserAvatar.vue

--- adminonefrontend/src/components/UserAvatar.vue
+++ adminonefrontend/src/components/UserAvatar.vue
@@ -1,136 +1,40 @@
 <script setup>
-import { reactive } from 'vue'
-import { useMainStore } from '@/stores/main'
-import { mdiAccount, mdiMail, mdiAsterisk, mdiFormTextboxPassword, mdiGithub } from '@mdi/js'
-import SectionMain from '@/components/SectionMain.vue'
-import CardBox from '@/components/CardBox.vue'
-import BaseDivider from '@/components/BaseDivider.vue'
-import FormField from '@/components/FormField.vue'
-import FormControl from '@/components/FormControl.vue'
-import FormFilePicker from '@/components/FormFilePicker.vue'
-import BaseButton from '@/components/BaseButton.vue'
-import BaseButtons from '@/components/BaseButtons.vue'
-import UserCard from '@/components/UserCard.vue'
-import LayoutAuthenticated from '@/layouts/LayoutAuthenticated.vue'
-import SectionTitleLineWithButton from '@/components/SectionTitleLineWithButton.vue'
+import { computed } from 'vue'
 
-const mainStore = useMainStore()
-
-const profileForm = reactive({
-  name: mainStore.userName,
-  email: mainStore.userEmail
+const props = defineProps({
+  username: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String,
+    default: null
+  },
+  api: {
+    type: String,
+    default: 'avataaars'
+  }
 })
 
-const passwordForm = reactive({
-  password_current: '',
-  password: '',
-  password_confirmation: ''
-})
+const avatar = computed(
+  () =>
+    props.avatar ??
+    `https://api.dicebear.com/7.x/${props.api}/svg?seed=${props.username.replace(
+      /[^a-z0-9]+/gi,
+      '-'
+    )}.svg`
+)
 
-const submitProfile = () => {
-  mainStore.setUser(profileForm)
-}
-
-const submitPass = () => {
-  //
-}
+const username = computed(() => props.username)
 </script>
 
 <template>
-  <LayoutAuthenticated>
-    <SectionMain>
-      <SectionTitleLineWithButton :icon="mdiAccount" title="个人资料" main>
-        <BaseButton
-          href="https://github.com/justboil/admin-one-vue-tailwind"
-          target="_blank"
-          :icon="mdiGithub"
-          label="在 GitHub 上点赞"
-          color="contrast"
-          rounded-full
-          small
-        />
-      </SectionTitleLineWithButton>
-
-      <UserCard class="mb-6" />
-
-      <div class="grid grid-cols-1 lg:grid-cols-2 gap-6">
-        <CardBox is-form @submit.prevent="submitProfile">
-          <FormField label="头像" help="最大 500kb">
-            <FormFilePicker label="上传" />
-          </FormField>
-
-          <FormField label="姓名" help="必填项。您的姓名">
-            <FormControl
-              v-model="profileForm.name"
-              :icon="mdiAccount"
-              name="username"
-              required
-              autocomplete="username"
-            />
-          </FormField>
-          <FormField label="邮箱" help="必填项。您的邮箱">
-            <FormControl
-              v-model="profileForm.email"
-              :icon="mdiMail"
-              type="email"
-              name="email"
-              required
-              autocomplete="email"
-            />
-          </FormField>
-
-          <template #footer>
-            <BaseButtons>
-              <BaseButton color="info" type="submit" label="提交" />
-              <BaseButton color="info" label="选项" outline />
-            </BaseButtons>
-          </template>
-        </CardBox>
-
-        <CardBox is-form @submit.prevent="submitPass">
-          <FormField label="当前密码" help="必填项。您的当前密码">
-            <FormControl
-              v-model="passwordForm.password_current"
-              :icon="mdiAsterisk"
-              name="password_current"
-              type="password"
-              required
-              autocomplete="current-password"
-            />
-          </FormField>
-
-          <BaseDivider />
-
-          <FormField label="新密码" help="必填项。新密码">
-            <FormControl
-              v-model="passwordForm.password"
-              :icon="mdiFormTextboxPassword"
-              name="password"
-              type="password"
-              required
-              autocomplete="new-password"
-            />
-          </FormField>
-
-          <FormField label="确认密码" help="必填项。再次输入新密码">
-            <FormControl
-              v-model="passwordForm.password_confirmation"
-              :icon="mdiFormTextboxPassword"
-              name="password_confirmation"
-              type="password"
-              required
-              autocomplete="new-password"
-            />
-          </FormField>
-
-          <template #footer>
-            <BaseButtons>
-              <BaseButton type="submit" color="info" label="提交" />
-              <BaseButton color="info" label="选项" outline />
-            </BaseButtons>
-          </template>
-        </CardBox>
-      </div>
-    </SectionMain>
-  </LayoutAuthenticated>
+  <div>
+    <img
+      :src="avatar"
+      :alt="username"
+      class="rounded-full block h-auto w-full max-w-full bg-gray-100 dark:bg-slate-800"
+    />
+    <slot />
+  </div>
 </template>
